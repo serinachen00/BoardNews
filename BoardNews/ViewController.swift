bd88932d8addf582290ceb13712f42c55cf0b937
serinachen00 <serina2serinaeva@gmail.com>
2020-05-08 23:06:55 -0700

    News UI implementation

--- BoardNews/ViewController.swift
+++ BoardNews/ViewController.swift
@@ -1,10 +1,8 @@
-//
 //  ViewController.swift
 //  BoardNews
 //
 //  Created by Serina Chen on 5/2/20.
 //  Copyright © 2020 Serina Chen. All rights reserved.
-//
 
 import UIKit
 
@@ -13,6 +11,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
